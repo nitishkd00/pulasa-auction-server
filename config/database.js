bd408b1801dfd5f4c23184512344c4eaa9681562
Nitish Kumar <nitishkumardevoju@gmail.com>
2025-08-14 17:38:36 +0530

    CRITICAL: Remove exposed MongoDB credentials and add security measures

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://nitishkumardevoju:muwLYrPn5blRAGCd@pulasa.sjvscku.mongodb.net/test?retryWrites=true&w=majority&appName=pulasa';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/pulasa-auction';
 
 const connectDatabase = async () => {
   try {
